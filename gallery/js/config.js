731b52401a95dcbb34dd7bf26b5666b6b7f21746
shiga yuichi <yuichi.shiga@gmail.com>
2014-10-25 15:54:13 -0700

    ver0.9.1wq

--- gallery/js/config.js
+++ gallery/js/config.js
@@ -2,8 +2,8 @@
 
 /* dropbox infomation */
 config = {
-	apiKey : 'fewg4ccms82y9eg',
-	token : 'OK-zP0XeWecAAAAAAAAkvjwvooSZO5cHL_8Nd0tE2adGyoqqU-_VDcdtR7F_bTFa', // get from app setting page. click "generate token button". do not share this token.
+	apiKey : 'qb0qlu8j36mik9a',
+	token : 'OK-zP0XeWecAAAAAAAAk75-EsK8v9lRYmQNlrlrVxyUxAC8PtndLlMKZ3oh8lotl', // get from app setting page. click "generate token button". do not share this token.
 	imageDir : 'images',
 	imageConvertedDir : 'processed'
 };
